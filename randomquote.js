f39eaf1e0b263c2c725de05788fd749fad6859e5
Azrhafel <francistellego@gmail.com>
2022-02-21 20:18:23 +0800

    Made updates on background and randomquote.js

--- randomquote.js
+++ randomquote.js
@@ -1,31 +1,15 @@
 const topQuote = document.querySelector(".topquote");
-var tqContent = topQuote.querySelector("h3");
-
-const quotenum = 7;
+var tqContent = topQuote.querySelector("h2");
 
 
+const quotenum = 6;
 
-function getquote() {
-    const q1 = tqContent.innerHTML =
-    "<h3>Not eveything in life is something you control, but what you can control is yourself</h3>";
-    const q2 = tqContent.innerHTML = "<h3> Test2 </h3>";
-    const q3 = tqContent.innerHTML = "<h3> Test3 </h3>";
-    const q4 = tqContent.innerHTML = "<h3> Test4 </h3>";
-    const q5 = tqContent.innerHTML = "<h3> Test5 </h3>";
-    const q6 = tqContent.innerHTML = "<h3> Test6 </h3>";
-}
+const quote = [`Not eveything in life is something you control, but what you can control is yourself`,`It takes pressure to make diamonds`,`Do what you need to for survival, just don't forget to live`,`Failure is just another step towards success`,`Your problems will only get more overwhelming if it's all you think about`,`Being weak is not a bad thing, it just means you have more time to grow`]
 
-// Creates a random number from 1-6 with a q before the number
+
 function genRandom() {
   const number = Math.floor(Math.random() * quotenum);
-  return "q"+number
+  tqContent.innerHTML = quote[number]
 }
 
 genRandom()
-
-function init() {
-  const randomNumber = genRandom();
-  getquote(randomNumber);
-}
-
-init();
