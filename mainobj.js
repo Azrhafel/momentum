c687a62f027dcd484f562188bd3916a5ac8a609c
Azrhafel <francistellego@gmail.com>
2022-02-24 04:57:58 +0800

    Updates on previous files and added mainobj.js

--- mainobj.js
+++ mainobj.js
@@ -1,43 +1,44 @@
-const form = document.querySelector(".js-form"),
-    input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings");
+const form = document.querySelector(".mainobj"),
+  input = form.querySelector("input"),
+  objective = document.querySelector(".js-maingoal");
 
-const USER_LS = "currentUser",
-    SHOWING_CN ="showing";
+const CurObj = "currentObjective",
+  SHOWING_CN = "showing";
 
-function saveName(text) {
-    localStorage.setItem(USER_LS, text);
+function saveGoal(text) {
+  localStorage.setItem(CurObj, text);
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const currentValue = input.value;
-    paintGreeting(currentValue);
-    saveName(currentValue);
+  event.preventDefault();
+  const currentValue = input.value;
+  displayGoal(currentValue);
+  saveGoal(currentValue);
 }
 
-function askForName() {
-    form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit);
-}
-    
-function paintGreeting(text) {
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Bon día, ${text}!`;
+function askForGoal() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
 }
 
-function loadName() {
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null) {
-        askForName();
-    } else {
-        paintGreeting(currentUser);
-    }
+function displayGoal(text) {
+  form.classList.remove(SHOWING_CN);
+  objective.classList.add(SHOWING_CN);
+  objective.innerText = `Today you're going to ${text}!`;
+}
+
+function loadGoal() {
+  const currentObjective = localStorage.getItem(CurObj);
+  if (currentObjective === null) {
+    askForGoal();
+  } else {
+    displayGoal(currentObjective);
+    input.remove()
+  }
 }
 
 function init() {
-    loadName();
+  loadGoal();
 }
 
 init();
--- randomquote.js
+++ randomquote.js
@@ -1,15 +1,20 @@
 const topQuote = document.querySelector(".topquote");
 var tqContent = topQuote.querySelector("h2");
 
-
 const quotenum = 6;
 
-const quote = [`Not eveything in life is something you control, but what you can control is yourself`,`It takes pressure to make diamonds`,`Do what you need to for survival, just don't forget to live`,`Failure is just another step towards success`,`Your problems will only get more overwhelming if it's all you think about`,`Being weak is not a bad thing, it just means you have more time to grow`]
-
+const quote = [
+  `"Not eveything in life is something you control, but what you can control is yourself"`,
+  `"It takes pressure to make diamonds"`,
+  `"Do what you need to for survival, just don't forget to live"`,
+  `"Failure is just another step towards success"`,
+  `"Your problems will only get more overwhelming if it's all you think about"`,
+  `"Being weak is not a bad thing, it just means you have more time to grow"`,
+];
 
 function genRandom() {
   const number = Math.floor(Math.random() * quotenum);
-  tqContent.innerHTML = quote[number]
+  tqContent.innerHTML = quote[number];
 }
 
-genRandom()
+genRandom();
